7e4202a48c1d71938d5401358771dee25bb23893
Mr.Kangs <gykang00@gmail.com>
2021-05-10 19:11:56 -0700

    Added MDL format

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import './App.css';
-
+import {Layout, Header, HeaderRow, Content, Tab, HeaderTabs} from "react-mdl"
 
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+<div style={{height: '300px', position: 'relative'}}>
+    <Layout fixedHeader fixedTabs>
+        <Header>
+            <HeaderRow title="Infobility" />
+            <HeaderTabs ripple activeTab={0} onChange={(tabId) => {}}>
+                <Tab>Tab1</Tab>
+                <Tab>Tab2</Tab>
+                <Tab>Tab3</Tab>
+            </HeaderTabs>
+        </Header>
+        <Content>
+            <div className="page-content">You can add logic to update the content of this container based on the "activeTab" receive in the `onChange` callback.</div>
+        </Content>
+    </Layout>
+            </div>
   );
 }
 
