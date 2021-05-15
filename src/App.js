8a02ec2c127d50d1d6f2981bd792d84d02d25149
ShaurGaur <shauryavrat@live.com>
2021-05-15 16:11:35 -0700

    created modules json file

--- src/App.js
+++ src/App.js
@@ -1,33 +1,53 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import Main from "./components/main";
-import {Layout, Header, Navigation, Content, Menu, MenuItem} from "react-mdl"
-import { Link } from "react-router-dom"
-
+import {
+    Layout,
+    Header,
+    Drawer,
+    Navigation,
+    Content,
+    Menu,
+    MenuItem,
+} from "react-mdl";
+import { Link } from "react-router-dom";
 
 function App() {
-  return (
-<div className="demo-big-content">
-    <Layout>
-        <Header scroll>
-            <Link to = "/"><h3>Infobility</h3></Link>
-            <Navigation>
-                <a href = "/aboutus">About Us</a>
-                <a id  = "Courses">Course</a>
-                <Menu target ="Courses">
-                    <MenuItem><Link to = "/adhd">ADHD</Link></MenuItem>
-                    <MenuItem><Link to = "/autism">Autism</Link></MenuItem>
-                    <MenuItem><Link to = "/advocacy">Advocacy</Link></MenuItem>
-                    <MenuItem><Link to = "/fun">Fun</Link></MenuItem>
-                </Menu>
-            </Navigation>
-        </Header>
-        <Content>
-            <Main />
-        </Content>
-    </Layout>
-</div>
-  );
+    return (
+        <div className="demo-big-content">
+            <Layout>
+                <Header title="Infobility" scroll>
+                    {/* <Link to="/">
+                        <h3>Infobility</h3>
+                    </Link> */}
+                    <Navigation>
+                        <Link to="/">Home</Link>
+                        <Link to="/aboutus">About Us</Link>
+                        <Link to="/" id="Courses">
+                            Courses
+                        </Link>
+                        <Menu target="Courses">
+                            <MenuItem>
+                                <Link to="/adhd">ADHD</Link>
+                            </MenuItem>
+                            <MenuItem>
+                                <Link to="/autism">Autism</Link>
+                            </MenuItem>
+                            <MenuItem>
+                                <Link to="/advocacy">Advocacy</Link>
+                            </MenuItem>
+                            <MenuItem>
+                                <Link to="/fun">Fun</Link>
+                            </MenuItem>
+                        </Menu>
+                    </Navigation>
+                </Header>
+                <Content>
+                    <Main />
+                </Content>
+            </Layout>
+        </div>
+    );
 }
 
 export default App;
