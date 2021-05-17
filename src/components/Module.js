03b0749d0987f18a54fef2a50a7e9cd64da97548
ShaurGaur <shauryavrat@live.com>
2021-05-17 15:52:30 -0700

    got modules working

---
+++ src/components/Module.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import {
+    Tab,
+    Tabs,
+    CardTitle,
+    CardActions,
+    Button,
+    Card,
+    CardText,
+    Grid,
+    Cell,
+} from "react-mdl";
+import { Link } from "react-router-dom";
+
+class Module extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    renderCard(elem) {
+        var url = "/modules/" + elem.title.replace(/\s+/g, "_");
+        return (
+            <Card shadow={5}>
+                <CardTitle
+                    style={{
+                        color: "cyan",
+                        fontweight: "bold",
+                        height: "200px",
+                    }}
+                >
+                    {elem.title}
+                </CardTitle>
+                <CardText>{elem.description}</CardText>
+                <CardActions>
+                    <Button>
+                        <Link to={url}>Open</Link>
+                    </Button>
+                    <Button>Preview</Button>
+                </CardActions>
+            </Card>
+        );
+    }
+
+    render() {
+        return (
+            <div className="module-page">
+                <h1>{this.props.info.title}</h1>
+                <p>{this.props.info.description || "No description here."}</p>
+                <div className="card-container">
+                    {this.props.info.submodules.map((elem) =>
+                        this.renderCard(elem)
+                    )}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Module;
--- src/components/main.js
+++ src/components/main.js
@@ -1,22 +1,64 @@
-import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-import HomePage from './HomePage';
-import AboutUs from './AboutUs';
+import HomePage from "./HomePage";
+import AboutUs from "./AboutUs";
 import ADHD from "./ADHD";
 import Autism from "./Autism";
 import Advocacy from "./Advocacy";
 import Fun from "./Fun";
+import Module from "./Module";
 
-const Main = () => (
-    <Switch>
-        <Route exact path = "/" component = {HomePage}/>
-        <Route path = "/aboutus" component = {AboutUs}/>
-        <Route path = "/adhd" component = {ADHD}/>
-        <Route path ="/autism" component = {Autism}/>
-        <Route path ="/advocacy" component = {Advocacy}/>
-        <Route path ="/fun" component = {Fun}/>
-    </Switch>
-)
+class Main extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            expandedNodes: this.buildArr(this.props.info, true),
+        };
+    }
+
+    buildArr(obj, root) {
+        var ans = [];
+
+        if (obj != undefined) {
+            ans.push({ title: obj.title, body: obj });
+
+            obj.submodules.forEach((elem) => {
+                ans = ans.concat(this.buildArr(elem, false));
+            });
+        }
+
+        console.log("Ans:", ans);
+        if (root) this.setState({ expandedNodes: ans });
+        return ans;
+    }
+
+    componentDidMount() {
+        // window.addEventListener("load", this.buildArr(this.props.info, true));
+        // this.setState({ expandedNodes: arr });
+    }
+
+    render() {
+        return (
+            <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route path="/aboutus" component={AboutUs} />
+                <Route path="/adhd" component={ADHD} />
+                <Route path="/autism" component={Autism} />
+                <Route path="/advocacy" component={Advocacy} />
+                <Route path="/fun" component={Fun} />
+                {this.state.expandedNodes.map((elem) => {
+                    var url = "/modules/" + elem.title.replace(/\s+/g, "_");
+                    return (
+                        <Route
+                            path={url}
+                            render={() => <Module info={elem.body} />}
+                        />
+                    );
+                })}
+            </Switch>
+        );
+    }
+}
 
 export default Main;
